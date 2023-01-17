55e36911e65d5d74f6a66e01f832da22b0047e6c
Ricardo Castellan <castellanra@gmail.com>
2023-01-17 20:39:37 +0000

    Initial structure

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+
+const port = Number(process.env.PORT);
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
